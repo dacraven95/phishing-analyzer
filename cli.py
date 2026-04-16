e33eee33c5983a0afbe5b83a928032494d2023c1
Daniel Craven <dacraven95@gmail.com>
2026-04-16 18:52:19 -0400

    Added email file validation pre analysis.

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import sys
 from phish_analyzer.core import run_analysis, run_analysis_and_pdf
 from datetime import datetime
 from halo import Halo
@@ -15,9 +16,34 @@ def main():
 
     args = parser.parse_args()
 
+    file_path = args.file
+
+    ### Email file validation before running analysis
+    # Checking if the file exists
+    if not os.path.exists(file_path):
+        print(f"[!] Error: File not found: '{file_path}'")
+        sys.exit(1)
+    
+    # Checking that we actually have a file (!a directory)
+    if not os.path.isfile(file_path):
+        print(f"[!] Error: Path is not a file: '{file_path}'")
+        sys.exit(1)
+
+    # Checking the file is readable
+    if not os.access(file_path, os.R_OK):
+        print(f"[!] Error: File is not readable (permission denied): '{file_path}'")
+        sys.exit(1)
+    
+    # Checking is a supported extension
+    ALLOWED_EXTENSIONS = {".eml", ".txt"}
+    _, ext = os.path.splitext(file_path)
+    if ext.lower() not in ALLOWED_EXTENSIONS:
+        print(f"[!] Error: Unsupported file type '{ext}'. Supported file types: {ALLOWED_EXTENSIONS}")
+        sys.exit(1)
+
     # Call run_analysis() and let it print
     if not args.report:
-        run_analysis(args.file, use_json=args.json)
+        run_analysis(file_path, use_json=args.json)
     else:
         now = datetime.now()
         filename_timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
@@ -25,7 +51,7 @@ def main():
 
         # Ensure the path specified exists before starting to generate the report PDF
         if args.output:
-            output_dir = os.path.dirname(pdf_path)
+            output_dir = os.path.dirname(os.path.abspath(pdf_path))
             os.makedirs(output_dir, exist_ok=True)
 
         print("Starting Analysis with PDF Report Generation...")
